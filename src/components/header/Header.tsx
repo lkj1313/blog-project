305dc8cfae7fb5866019db21a9585737015a4d3f
lkj1313 <smm45108@gmail.com>
2025-08-24 21:33:37 +0900

    feat(home): 메인페이지 초기 UI 구현

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,7 +5,7 @@ import HeaderDesktopActions from "./HeaderDesktopActions";
 
 const Header = () => {
   return (
-    <header>
+    <header className="">
       <div className="fixed top-0 left-0 z-40 h-[60px] w-full bg-background-default">
         <div className="size-full">
           <div className="container flex h-full items-center justify-between *:h-full">
@@ -13,13 +13,13 @@ const Header = () => {
             <HeaderMobileActions />
             <HeaderDesktopActions />
           </div>
-          <div
-            data-orientation="horizontal"
-            role="none"
-            className="shrink-0 !h-px w-full bg-line-200"
-          ></div>
         </div>
       </div>
+      <div
+        data-orientation="horizontal"
+        role="none"
+        className="shrink-0 block h-header bg-background-default"
+      ></div>
     </header>
   );
 };
